11b561b0983856e86c7a0bab84ecab121d6af122
nesmanpro <nesmanpro@gmail.com>
2023-12-05 19:35:34 +0100

    everything done

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 // Primer desafio entregable -- Back-End--
 
 class ProductManager {
+
+
+    // Constructor con array vacio
     constructor() {
-        this.product = [];
-        this.productId = 1;
+        this.products = [];
+        this.productId = 0;
     }
 
     addProduct(title, description, price, thumbnail, code, stock) {
 
-        // Validar campos obligatorios
+        // Valida campos obligatorios
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             console.log('Te faltó uno de los campos, recordá que todos son obligatorios');
             return;
         }
 
-        // Validar código único
-        if (this.product.some(item => item.code === code)) {
-            console.log('Vaya!, parece que el codigo esta repetido.')
+        // Valida código único
+        if (this.products.some(item => item.code === code)) {
+            console.log('Vaya!, parece que el codigo esta repetido y debe ser unico.');
+            return;
         }
 
-        // Crear producto con id autoincrementable
+        // Crea nuevo objeto con datos anteriores y id autoincrementable
         const newProduct = {
-            id: this.productId++,
+            id: ++this.productId,
             title,
             description,
             price,
@@ -29,7 +33,43 @@ class ProductManager {
             code,
             stock
         }
+
+        // Agrega el nuevo producto al array
+        this.products.push(newProduct);
     }
 
+    getProduct() {
+        console.log(this.products)
+    }
+
+    getProductById(id, title) {
+        const prod = this.products.find(item => item.id === id);
+
+        if (prod) {
+            console.log(`Genial! Se entontró el producto: ${prod.title} `, prod);
+        } else {
+            console.log('Vaya! No hemos encontrado el producto que buscas');
+        }
+        return prod;
+
+
+    }
 
 }
+
+
+// Testing 
+
+const manager = new ProductManager();
+
+manager.getProduct();
+
+manager.addProduct("Yerba", "de la mejor", 10, "Sin imagen", "abc123", 25);
+manager.addProduct("Arroz", "de sushi", 10, "Sin imagen", "abc124", 25);
+manager.addProduct("Jamon", "pata negra", 10, "Sin imagen", "abc125", 25);
+
+
+manager.getProduct();
+
+manager.getProductById(7)
+

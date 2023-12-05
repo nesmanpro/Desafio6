cf276ba89f0c38519ce01a077dbfb9aec2ef28c6
nesmanpro <nesmanpro@gmail.com>
2023-12-05 17:59:08 +0100

    class y add product done

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
+// Primer desafio entregable -- Back-End--
+
 class ProductManager {
     constructor() {
         this.product = [];
+        this.productId = 1;
     }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+
+        // Validar campos obligatorios
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.log('Te faltó uno de los campos, recordá que todos son obligatorios');
+            return;
+        }
+
+        // Validar código único
+        if (this.product.some(item => item.code === code)) {
+            console.log('Vaya!, parece que el codigo esta repetido.')
+        }
+
+        // Crear producto con id autoincrementable
+        const newProduct = {
+            id: this.productId++,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+    }
+
+
 }
